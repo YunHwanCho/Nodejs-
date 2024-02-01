6135708502b6dcee3b41cc3201fa73d16ddc00d3
조윤환 <jyh6314@naver.com>
2024-02-01 18:20:12 +0900

    수정 삭제 기능 추가하기 with ajax

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs')
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 const { MongoClient } = require("mongodb");
+const methodOverride = require('method-override')
+app.use(methodOverride('_method')) 
 
 let db;
 const url =
@@ -94,8 +96,55 @@ app.get('/detail/:id', async (요청, 응답) => {
 
 })
 
+app.get('/edit/:id' , async(요청,응답)=>{
+  let result = await db.collection('post').findOne({ _id : new ObjectId(요청.params.id) })
+
+  응답.render('edit.ejs', {result: result})
+})
+
+app.post('/newpost', async(요청,응답)=> {
+
+  
 
 
+
+})
+
+app.post('/repost', async (요청, 응답) => {
+  try{
+    const postId = new ObjectId(요청.body.id);
+    const originalPost = await db.collection('post').findOne({ _id: postId });
+
+    if (!originalPost) {
+      응답.send('Post not found.'); 
+      return;
+    }
+    if(요청.body.title == '' && 요청.body.content == ''){
+      응답.send('내용을 정확하게 입력해주세요')
+    }else if(요청.body.id == ''){
+      응답.send('id가 변경되었습니다.')
+
+    }
+    else{
+      await db.collection('post').updateOne( { _id : new ObjectId(요청.body.id) }, {$set: {title : 요청.body.title, content : 요청.body.content }}) 
+      응답.redirect('/list')
+  
+    }
+   
+
+  }catch(err){
+    응답.status(500).send('서버 에러')
+
+  }
+
+})
+//ajax의 장점은 새로고침이 필요없음
+
+app.delete('/delete', async(요청,응답)=>{
+  console.log(요청.query)
+  await db.collection('post').deleteOne({ _id : new ObjectId(요청.query.docid)})
+  응답.send('삭제완료')
+})
 
 
 
